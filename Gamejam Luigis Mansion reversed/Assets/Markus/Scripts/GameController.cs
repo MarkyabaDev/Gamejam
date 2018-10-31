d984b65a3540ac0db3fa6860152527ed0bff0a7c
Markus <mbaars@web.de>
2018-10-31 22:17:55 +0100

    FPSPlayer changes

---
+++ Gamejam Luigis Mansion reversed/Assets/Markus/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public List<GameObject> shortcuts;
+    public List<string> shortcutNames;
+
+    public static GameController gameControllerInstance;
+
+    private void Awake()
+    {
+        if (gameControllerInstance == null)
+
+            gameControllerInstance = this;
+
+        else if (gameControllerInstance != this)
+
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        shortcuts.AddRange(GameObject.FindGameObjectsWithTag("Shortcut"));
+        foreach(GameObject go in shortcuts)
+        {
+            shortcutNames.Add(go.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
