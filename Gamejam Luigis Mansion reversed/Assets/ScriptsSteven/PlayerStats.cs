c10077016d2928d36bfbc14d87a09a32605e7570
oceanstevy <44651676+oceanstevy@users.noreply.github.com>
2018-10-31 20:50:45 +0100

    Playermove + enemy

---
+++ Gamejam Luigis Mansion reversed/Assets/ScriptsSteven/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int lives = 5;
+    public Text livesText;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        livesText.text = "Lives: " + lives;
+    }
+}
