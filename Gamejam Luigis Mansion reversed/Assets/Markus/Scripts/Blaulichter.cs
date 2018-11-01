89128944c3c0b5f4817e6f95825545bec9b7529c
Markus <mbaars@web.de>
2018-11-01 19:56:29 +0100

    Post Process and Effekts

---
+++ Gamejam Luigis Mansion reversed/Assets/Markus/Scripts/Blaulichter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaulichter : MonoBehaviour {
+
+    public List<GameObject> lights;
+    public float rotSpeed = 2;
+	// Use this for initialization
+	void Start ()
+    {
+        lights.AddRange(GameObject.FindGameObjectsWithTag("Blaulicht"));
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        RotateAllLights();
+	}
+
+    void RotateAllLights()
+    {
+        foreach (GameObject go in lights)
+        {
+            go.transform.eulerAngles += new Vector3(0, Time.deltaTime * rotSpeed, 0);
+        }
+    }
+}
