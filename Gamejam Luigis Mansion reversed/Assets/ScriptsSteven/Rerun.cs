1158b6bdf37ea3b690ccb33f2d279d14ece79705
oceanstevy <44651676+oceanstevy@users.noreply.github.com>
2018-11-01 04:30:49 +0100

    SpawnPoints + UI

---
+++ Gamejam Luigis Mansion reversed/Assets/ScriptsSteven/Rerun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rerun : MonoBehaviour {
+
+    public GameObject pauseOverlay;
+    public GameObject player;
+    public GameObject SpawnPoint;
+
+    public void RunAgain()
+    {
+        pauseOverlay.SetActive(false);
+        Time.timeScale = 1;
+        player.transform.position = new Vector3(SpawnPoint.transform.position.x, SpawnPoint.transform.position.y, SpawnPoint.transform.position.z);
+    }
+}
---
+++ Gamejam Luigis Mansion reversed/Assets/ScriptsSteven/SetSpawnPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetSpawnPoint : MonoBehaviour {
+
+    public GameObject SpawnMain;
+    public GameObject SpawnGarage;
+    public GameObject SpawnBack;
+    public GameObject ChangeSpawnTo;
+    public GameObject SpawnPoint;
+
+    public List<GameObject> SelectSpawn;
+    private string spawnName;
+
+    private void Start()
+    {
+        
+    }
+
+    public void UseSpawnPoint()
+    {
+        SelectSpawn.AddRange(GameObject.FindGameObjectsWithTag("SpawnPointSelect"));
+        foreach (GameObject child in SelectSpawn)
+        {
+            if (child.transform.GetComponent<Toggle>().isOn)
+            {
+                spawnName = child.transform.GetChild(1).GetComponent<Text>().text;
+                ChangeSpawnTo = GameObject.Find("Spawnpoints/"+spawnName);
+                Debug.Log("Spawnpoints/"+spawnName);
+                SpawnPoint.transform.position = new Vector3(ChangeSpawnTo.transform.position.x, ChangeSpawnTo.transform.position.y, ChangeSpawnTo.transform.position.z);
+            }
+        }
+    }
+}
