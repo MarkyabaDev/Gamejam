9bbe5a08bcec9f95a05cd04e1b47adc4aa0bc81d
WillDores93 <44651698+WillDores93@users.noreply.github.com>
2018-11-02 01:24:13 +0100

    Menus (controls and so)
    New Shiiiittttttt

---
+++ Gamejam Luigis Mansion reversed/Assets/Will/PreGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreGame : MonoBehaviour
+{
+    public void Pregame()
+    {
+        SceneManager.LoadScene("PreGame");
+    }
+
+}
